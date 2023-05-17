f2d87ddd97566ccc00678a859c99e072338779ed
gahee99 <u1879@sookmyung.ac.kr>
2023-05-17 16:33:53 +0900

    초기 세팅

---
+++ src/test/java/org/sopt/myhouse/MyhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.myhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
